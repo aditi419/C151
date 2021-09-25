63bc02e23ef14ee07b39dd65b012d07c534a4e5e
aditi419 <76972940+aditi419@users.noreply.github.com>
2021-09-25 16:31:52 -0400

    Add files via upload

---
+++ C160.py
@@ -0,0 +1,46 @@
+from tkinter import *
+from tkinter import filedialog
+import os
+from PIL import ImageTk,Image
+root = Tk()
+root.minsize(650,650)
+root.maxsize(650,650)
+
+open_image = ImageTk.PhotoImage(Image.open('open.png'))
+save_image = ImageTk.PhotoImage(Image.open('save.png'))
+exit_image = ImageTk.PhotoImage(Image.open('exit.jpg'))
+
+label_file_name = Label(root,text='File Name:')
+label_file_name.place(relx=0.28,rely=0.03,anchor=CENTER)
+
+input_file_name = Entry(root)
+input_file_name.place(relx=0.5,rely=0.03,anchor=CENTER)
+
+my_text = Text(root,height=35,width=80)
+my_text.place(relx=0.5,rely=0.5,anchor=CENTER)
+
+name = ''
+def openFile():
+    global name
+    my_text.delete(1.0,END)
+    input_file_name.delete(0,END)
+    text_file = filedialog.askopenfilename(title='Open Text File',filetypes=(('text files','*.txt'),))
+    print(text_file)
+    name = os.path.basename(text_file)
+    formatted_name = name.split('.')[0]
+    input_file_name.insert(END,formatted_name)
+    root.title(formatted_name)
+    text_file = open(name,'r')
+    paragraph = text_file.read()
+    my_text.insert(END,paragraph)
+    text_file.close()
+
+openBtn = Button(root,image=open_image,text = 'Open File',command=openFile)
+#saveBtn = Button(root,image=save_image,text = 'Save File',command=saveFile)
+#exitBtn = Button(root,image=exit_image,text = 'Exit File',command=exitFile)
+
+openBtn.place(relx=0.1,rely=0.03,anchor=CENTER)
+#saveBtn.place(relx=0.15,rely=0.03,anchor=CENTER)
+#exitBtn.place(relx=0.2,rely=0.03,anchor=CENTER)
+
+root.mainloop()
